1b5ea44a33bea3aca0525ecca294a8744dd3bcc7
Mike Noel <mike@vnoel.com>
2026-05-01 15:35:45 -0700

    moving firebase config to env var

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,13 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBseo9xfv5dd8e_0G47qm_bUNUW42fXd_Y",
-  authDomain: "it-takes-two-5794c.firebaseapp.com",
-  projectId: "it-takes-two-5794c",
-  storageBucket: "it-takes-two-5794c.firebasestorage.app",
-  messagingSenderId: "18956015188",
-  appId: "1:18956015188:web:1f05f05a552a064789bfc1"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
